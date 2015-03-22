9e36afdfc39d8cc2aacbbc7290bd8b09f8771d84
Takeru Chuganji <takeru@hoppenichu.com>
2015-03-22 19:02:34 +0900

    Support Mac OS 10.9

--- Example/Example-Mac/AppDelegate.h
+++ Example/Example-Mac/AppDelegate.h
@@ -2,14 +2,12 @@
 //  AppDelegate.h
 //  Example-Mac
 //
-//  Created by Takeru Chuganji on 1/4/15.
+//  Created by Takeru Chuganji on 3/22/15.
 //
 //
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
-
 @end
 
